d40e256ba06d76340c039e0701ce961ca4129eb3
unknown <nawresncib@insat.u-carthage.tn>
2021-06-30 13:16:30 +0100

    product and command  model

--- server/model/model.js
+++ server/model/model.js
@@ -2,20 +2,61 @@
 const mongoose = require('mongoose');
 
 var user = new mongoose.Schema({
-    username : {
-        type : String,
+    username: {
+        type: String,
         required: true,
         unique: true
     },
-    role : String,
-    password : {
-        type : String,
+    role: String,
+    password: {
+        type: String,
         required: true,
     },
-    createdAt : {
-        type : Date,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+const Usermd = mongoose.model('usermd', user);
+exports.Usermd=Usermd;
+
+var product = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    category: String,
+    unit: String,
+    price: Number,
+    inventory: Number,
+    users_ratings: [{
+        username: String,
+        rating: Number
+    }],
+    average_rating: Numbers
+
+})
+const productmd = mongoose.model('productmd', product);
+exports.productmd=productmd;
+
+var command = new mongoose.Schema({
+    username: {
+        type: String,
+    },
+    products: [productId],
+    total_price: Number,
+    status: String,
+    createdAt: {
+        type: Date,
         default: Date.now
     }
 })
 
-const Usermd = mongoose.model('usermd', user);
+const commandmd = mongoose.model('commandmd', command);
+exports.commandmd=commandmd;
