73dcb6c390cf8c08d011279f80d5c669bb149a3a
nawresncib12 <nawresncib@insat.u-carthage.tn>
2021-07-18 10:46:52 +0100

    fix error

--- server/controller/ProductController.js
+++ server/controller/ProductController.js
@@ -70,69 +70,68 @@ exports.deletProduct = (req, res) => {
     }
     //list products 
 exports.listProducts = async(req, res) => {
-    var minPrice, maxPrice;
-    filters = { is_deleted: false };
-    for (var key in req.body) {
-        switch (key) {
-            case "sort":
-                sort = req.body.sort;
-                break;
-            case "minPrice":
-                minPrice = req.body[key];
-                break;
-            case "maxPrice":
-                maxPrice = req.body[key];
-                break;
-            case "category":
-            case "in-stock":
-            case "name":
-                filters[key] = req.body[key];
-                break;
-            default:
-                res.status(404).send({ message: `unaccepted filter` })
+        var minPrice, maxPrice;
+        filters = { is_deleted: false };
+        for (var key in req.body) {
+            switch (key) {
+                case "sort":
+                    sort = req.body.sort;
+                    break;
+                case "minPrice":
+                    minPrice = req.body[key];
+                    break;
+                case "maxPrice":
+                    maxPrice = req.body[key];
+                    break;
+                case "category":
+                case "in-stock":
+                case "name":
+                    filters[key] = req.body[key];
+                    break;
+                default:
+                    res.status(404).send({ message: `unaccepted filter` })
+            }
+
         }
 
-    }
+        if (minPrice && maxPrice) {
+            filters['unit_price'] = { $gt: minPrice, $lt: maxPrice };
+        } else if (minPrice) {
+            filters['unit_price'] = { $gt: minPrice };
+        } else if (maxPrice) {
+            filters['unit_price'] = { $lt: maxPrice };
+        }
 
-    if (minPrice && maxPrice) {
-        filters['unit_price'] = { $gt: minPrice, $lt: maxPrice };
-    } else if (minPrice) {
-        console.log("heere")
-        filters['unit_price'] = { $gt: minPrice };
-    } else if (maxPrice) {
-        filters['unit_price'] = { $lt: maxPrice };
-    }
+        switch (sort) {
+            case "Latest":
+                sort = { createdAt: -1 }
+                break;
+            case "Cheapest":
+                sort = { unit_price: 1 }
+                break;
+            case "Most expensive":
+                sort = { unit_price: -1 }
+                break;
+            case "Best rated":
+                sort = { average_rating: -1 }
+                break;
+            default:
+                sort = { createdAt: -1 }
+        }
 
-    switch (sort) {
-        case "Latest":
-            sort = { createdAt: -1 }
-            break;
-        case "Cheapest":
-            sort = { unit_price: 1 }
-            break;
-        case "Most expensive":
-            sort = { unit_price: -1 }
-            break;
-        case "Best rated":
-            sort = { average_rating: -1 }
-            break;
-        default:
-            sort = { createdAt: -1 }
-    }
-}
 
-try {
-    const products = await ProductModel.find(filters).sort(sort);
-    if (!products.length) {
-        res.status(404).send({ message: `no products found` })
-    } else {
-        res.send(`Here is your list \n ${products}`)
+        try {
+            const products = await ProductModel.find(filters).sort(sort);
+            if (!products.length) {
+                res.status(404).send({ message: `no products found` })
+            } else {
+                res.send(`Here is your list \n ${products}`)
+            }
+        } catch (err) {
+            return res.status(500).send({ message: err.mesesage })
+        }
     }
-} catch (err) {
-    return res.status(500).send({ message: err.mesesage })
-}
-}
-//rate Product
+    //rate Product
 exports.rateProduct = async(req, res) => {
     const productId = req.params.productId;
     const userId = req.user._id;
